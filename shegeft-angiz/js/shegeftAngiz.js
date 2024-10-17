6c0c4ca76a1d9b2283ded41682afa95a0dc1efe4
Mohamadreza <154428875+MohammadrezaMoradiMokaram@users.noreply.github.com>
2024-10-17 20:38:37 +0330

    Added via uploading
    shegeft-angiz/js+html

---
+++ shegeft-angiz/js/shegeftAngiz.js
@@ -0,0 +1,56 @@
+var swiper = new Swiper(".myAmazingOffer", {
+    slidesPerView: 1,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  var swiper = new Swiper(".myDailyAO", {
+    slidesPerView: 1.98,
+    spaceBetween: 15,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });  
+
+  var swiper = new Swiper(".myCategory", {
+    slidesPerView: 11.5,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySelectedAO", {
+    slidesPerView: 8.3,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".myEndingAo", {
+    slidesPerView: 7.80,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".myCustomAo", {
+    slidesPerView: 7.80,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
